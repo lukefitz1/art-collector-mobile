f742a2d86bce20ad7722743f0b691c0ca88f6978
lukefitz1 <lukefitz1@gmail.com>
2019-11-22 20:05:49 -0700

    adding in some basic styling to sign in screen

---
+++ src/components/Spacer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { SplashScreen } from 'expo';
+
+const Spacer = ({ children }) => {
+  return <View style={styles.spacer}>{children}</View>
+};
+
+const styles = StyleSheet.create({
+  spacer: {
+    margin: 15
+  }
+});
+
+export default Spacer;
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button, Image } from 'react-native-elements';
+import Spacer from '../components/Spacer'
 
 const SignInScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SignInScreen</Text>
-      <Button title="Go to Customers" onPress={() => navigation.navigate('CustomerList')}/>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../../assets/spire-logo.png')} />
+      <Spacer><Text h3>Log In</Text></Spacer>
+      <Input label="Username" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer><Button style={styles.button} title="Log In" onPress={() => navigation.navigate('CustomerList')}/></Spacer>
     </View>
   )
 };
 
-const styles = StyleSheet.create({});
+SignInScreen.navigationOptions = () => {
+  return {
+    header: null
+  };
+};
+const styles = StyleSheet.create({
+  container: {
+    borderColor: 'red',
+    borderWidth: 10,
+    flex: 1,
+    justifyContent: 'center',
+    // marginBottom: 150
+    alignItems: 'center'
+  },
+  logo: {
+    height: 150,
+    width: 100
+  },
+  button: {
+    alignItems: 'stretch'
+  }
+});
 
 export default SignInScreen;

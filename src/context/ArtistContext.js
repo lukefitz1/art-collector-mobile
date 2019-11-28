17fa3722af836901b899aeebe8e1ab09964d3ad4
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-11-28 09:51:35 -0700

    wiring up the create artist page

--- src/context/ArtistContext.js
+++ src/context/ArtistContext.js
@@ -22,10 +22,46 @@ const getArtists = dispatch => {
   };
 };
 
+const addArtist = () => {
+  return async (
+    firstName,
+    lastName,
+    biography,
+    additionalInfo,
+    artistImage,
+    callback
+  ) => {
+    try {
+      await spireApi.post("/artist", {
+        firstName,
+        lastName,
+        biography,
+        additionalInfo,
+        artist_image: artistImage
+      });
+
+      // TODO: Instead of following dispatch, we may need to re-fetch all customers
+      // so that we can navigate to the detail page of the new customer after creation
+      // dispatch({ type: "add_customer", payload: response.data });
+
+      // So, for example (from the get customers function above):
+      // const response = await spireApi.get("/customer");
+      // dispatch({ type: "get_customers", payload: response.data });
+
+      if (callback) {
+        callback();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   artistReducer,
   {
-    getArtists
+    getArtists,
+    addArtist
   },
   []
 );
--- src/screens/artist/ArtistCreateScreen.js
+++ src/screens/artist/ArtistCreateScreen.js
@@ -10,13 +10,81 @@ import {
 import { Context as ArtistContext } from "../../context/ArtistContext";
 
 const ArtistCreateScreen = ({ navigation }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [biography, setBiography] = useState("");
+  const [additionalInfo, setAdditionalInfo] = useState("");
+  const [artistImage, setArtistImage] = useState("");
+  const { addArtist } = useContext(ArtistContext);
+
   return (
-    <View>
-      <Text>ArtistCreateScreen</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.label}>First Name:</Text>
+        <TextInput
+          style={styles.input}
+          value={firstName}
+          onChangeText={text => setFirstName(text)}
+        />
+        <Text style={styles.label}>Last Name:</Text>
+        <TextInput
+          style={styles.input}
+          value={lastName}
+          onChangeText={text => setLastName(text)}
+        />
+        <Text style={styles.label}>Biography:</Text>
+        <TextInput
+          style={styles.input}
+          value={biography}
+          onChangeText={text => setBiography(text)}
+        />
+        <Text style={styles.label}>Additional Information:</Text>
+        <TextInput
+          style={styles.input}
+          value={additionalInfo}
+          onChangeText={text => setAdditionalInfo(text)}
+        />
+        <Text style={styles.label}>Artist Image:</Text>
+        <TextInput
+          style={styles.input}
+          value={artistImage}
+          onChangeText={text => setArtistImage(text)}
+        />
+        <Button
+          title="Add New Artist"
+          onPress={() =>
+            addArtist(
+              firstName,
+              lastName,
+              biography,
+              additionalInfo,
+              artistImage,
+              () => {
+                navigation.navigate("ArtistList");
+              }
+            )
+          }
+        />
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5
+  }
+});
 
 export default ArtistCreateScreen;

715d6844952f20c6cabc08e900d35c717b9bcc3d
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-11-28 09:32:26 -0700

    adding the create collection screen

--- src/context/CollectionContext.js
+++ src/context/CollectionContext.js
@@ -22,10 +22,37 @@ const getCollections = dispatch => {
   };
 };
 
+const addCollection = () => {
+  return async (collectionName, identifier, year, callback) => {
+    try {
+      await spireApi.post("/collections", {
+        collectionName,
+        identifier,
+        year
+      });
+
+      // TODO: Instead of following dispatch, we may need to re-fetch all customers
+      // so that we can navigate to the detail page of the new customer after creation
+      // dispatch({ type: "add_customer", payload: response.data });
+
+      // So, for example (from the get customers function above):
+      // const response = await spireApi.get("/customer");
+      // dispatch({ type: "get_customers", payload: response.data });
+
+      if (callback) {
+        callback();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   collectionReducer,
   {
-    getCollections
+    getCollections,
+    addCollection
   },
   []
 );
--- src/screens/collection/CollectionCreateScreen.js
+++ src/screens/collection/CollectionCreateScreen.js
@@ -10,13 +10,60 @@ import {
 import { Context as CollectionContext } from "../../context/CollectionContext";
 
 const CollectionCreateScreen = ({ navigation }) => {
+  const [collectionName, setCollectionName] = useState("");
+  const [identifier, setIdentifier] = useState("");
+  const [year, setYear] = useState("");
+  const { addCollection } = useContext(CollectionContext);
+
   return (
-    <View>
-      <Text>CollectionCreateScreen</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.label}>Collection Name:</Text>
+        <TextInput
+          style={styles.input}
+          value={collectionName}
+          onChangeText={text => setCollectionName(text)}
+        />
+        <Text style={styles.label}>Collection Identifier:</Text>
+        <TextInput
+          style={styles.input}
+          value={identifier}
+          onChangeText={text => setIdentifier(text)}
+        />
+        <Text style={styles.label}>Year:</Text>
+        <TextInput
+          style={styles.input}
+          value={year}
+          onChangeText={text => setYear(text)}
+        />
+        <Button
+          title="Add New Collection"
+          onPress={() =>
+            addCollection(collectionName, identifier, year, () => {
+              navigation.navigate("CollectionList");
+            })
+          }
+        />
+      </ScrollView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+    margin: 5
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5
+  }
+});
 
 export default CollectionCreateScreen;

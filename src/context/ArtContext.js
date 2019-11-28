28fcf900d9a3f5fd2cef1ea409dbf6d3d247959d
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-11-28 10:41:20 -0700

    wiring up the create art screen

--- src/context/ArtContext.js
+++ src/context/ArtContext.js
@@ -22,10 +22,102 @@ const getArt = dispatch => {
   };
 };
 
+const addArtwork = () => {
+  return async (
+    objectId,
+    artType,
+    title,
+    date,
+    medium,
+    image,
+    description,
+    dimensions,
+    frameDimensions,
+    condition,
+    currentLocation,
+    source,
+    dateAcquired,
+    amountPaid,
+    currentValue,
+    notes,
+    notesImage,
+    additionalInfoLabel,
+    additionalInfoText,
+    additionalInfoImage,
+    additionalPdf,
+    reviewedBy,
+    reviewedDate,
+    provenance,
+    artist,
+    collection,
+    customer,
+    dateAcquiredLabel,
+    notesImageTwo,
+    additionalInfoImageTwo,
+    generalInformation,
+    showGeneralInfo,
+    customTitle,
+    callback
+  ) => {
+    try {
+      await spireApi.post("/artwork", {
+        ojbId: objectId,
+        artType,
+        title,
+        date,
+        medium,
+        image,
+        description,
+        dimensions,
+        frame_dimensions: frameDimensions,
+        condition,
+        currentLocation,
+        source,
+        dateAcquired,
+        amountPaid,
+        currentValue,
+        notes,
+        notesImage,
+        additionalInfoLabel,
+        additionalInfoText,
+        additionalInfoImage,
+        additionalPdf,
+        reviewedBy,
+        reviewedDate,
+        provenance,
+        artist_id: artist,
+        collection_id: collection,
+        customer_id: customer,
+        dateAcquiredLabel,
+        notesImageTwo,
+        additionalInfoImageTwo,
+        general_information_id: generalInformation,
+        show_general_info: showGeneralInfo,
+        custom_title: customTitle
+      });
+
+      // TODO: Instead of following dispatch, we may need to re-fetch all customers
+      // so that we can navigate to the detail page of the new customer after creation
+      // dispatch({ type: "add_customer", payload: response.data });
+
+      // So, for example (from the get customers function above):
+      // const response = await spireApi.get("/customer");
+      // dispatch({ type: "get_customers", payload: response.data });
+
+      if (callback) {
+        callback();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   artReducer,
   {
-    getArt
+    getArt,
+    addArtwork
   },
   []
 );

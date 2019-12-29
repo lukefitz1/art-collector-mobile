a43d68a043e309a615e0cbe28184ce4a61343aa5
Luke Fitzgerald <lukefitzgerald@nutrien.com>
2019-12-29 13:53:39 -0700

    adding in functionality to navigate to sign in when user receives 401 unauthorized response from request

--- src/api/spire.js
+++ src/api/spire.js
@@ -2,15 +2,17 @@ import axios from "axios";
 import { AsyncStorage } from "react-native";
 import Config from "../../config/config";
 import ENV from "../../env";
+import { navigate } from "../navigationRef";
 
 const url = Config.BASE_URL;
-console.log(`URL for local testing: ${url}`);
+// console.log(`URL for local testing: ${url}`);
 
 // const base_url = ENV.BASE_URL;
 // console.log(`URL for local testing 2: ${base_url}`);
 
 const instance = axios.create({
-  baseURL: "https://spire-art-services.herokuapp.com/api"
+  baseURL: "https://spire-art-services.herokuapp.com/api/"
+  // baseURL: "http://localhost:3000/api/"
 });
 
 instance.interceptors.request.use(
@@ -26,6 +28,19 @@ instance.interceptors.request.use(
   },
   err => {
     return Promise.reject(err);
+  }
+);
+
+instance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    if (error.response.status === 401) {
+      await AsyncStorage.removeItem("token");
+      navigate("SignIn");
+    }
+    return error;
   }
 );
 
